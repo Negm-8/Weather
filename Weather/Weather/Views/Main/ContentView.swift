09eaa453566329bef49eacebd798df9bf25fa32b
abdallah negm <113768788+Negm-8@users.noreply.github.com>
2023-07-11 15:06:40 +0300

    Add files via upload

---
+++ Weather/Weather/Views/Main/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by abdallah negm on 25/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by abdallah negm on 25/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
